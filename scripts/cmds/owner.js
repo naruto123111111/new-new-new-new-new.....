15f710b5d57527ac9d6023a11755e53e9209fea7
naruto123111111 <rutona442@gmail.com>
2026-08-27 11:45:47 +0600

    Update owner information and image link

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "owner",
     version: "1.3.0",
-    author: "Mᴏʜᴀᴍᴍᴀᴅ Aᴋᴀsʜ",
+    author: "亗•𝘔𝘈𝘔𝘜𝘕✿᭄",
     role: 0,
     shortDescription: "Owner information with image",
     category: "Information",
@@ -17,26 +17,33 @@ module.exports = {
 
   onStart: async function ({ api, event }) {
     const ownerText = 
-`╭─ 👑 Oᴡɴᴇʀ Iɴғᴏ 👑 ─╮
-│ 👤 Nᴀᴍᴇ       : Mᴏʜᴀᴍᴍᴀᴅ Aᴋᴀsʜ
-│ 🧸 Nɪᴄᴋ       : Aᴋᴀsʜ
-│ 🎂 Aɢᴇ        : 18+
-│ 💘 Rᴇʟᴀᴛɪᴏɴ : Sɪɴɢʟᴇ
-│ 🎓 Pʀᴏғᴇssɪᴏɴ : Sᴛᴜᴅᴇɴᴛ
-│ 📚 Eᴅᴜᴄᴀᴛɪᴏɴ : Iɴᴛᴇʀ 2ɴᴅ Yᴇᴀʀ
-│ 🏡 Lᴏᴄᴀᴛɪᴏɴ : 𝐃𝐡𝐚𝐤𝐚 - 𝐆𝐚𝐳𝐢𝐩𝐮𝐫
-├─ 🔗 Cᴏɴᴛᴀᴄᴛ ─╮
-│ 📘 Facebook  : fb.com/akashx404 
-│ 💬 Messenger: m.me/akashx404 
-│ 📞 WhatsApp  : wa.me/01933165880
-╰────────────────╯`;
+`┌───────────────⭓
+│ 𝗢𝗪𝗡𝗘𝗥 𝗗𝗘𝗧𝗔𝗜𝗟𝗦
+├───────────────
+│ 👤𝐍𝐚𝐦𝐞 : 𝐌𝐃 𝐇𝐀𝐌𝐈𝐌
+│ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫 : 𝐌𝐚𝐥𝐞
+│ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧 : 𝐈𝐧 𝐀 𝐎𝐫𝐠𝐢𝐧𝐚𝐥 𝐏𝐮𝐫𝐞 𝐒𝐢𝐧𝐠𝐞𝐥
+│ 🎂 𝐀𝐠𝐞 : 𝟏𝟖+
+│ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧 : 𝐈𝐬𝐥𝐚𝐦
+│ 🎓 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐒𝐢𝐧𝐠𝐞𝐥 𝐏𝐚𝐬𝐬 (𝟐𝟎𝟐𝟔)
+│ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : 𝐍𝐨𝐚𝐤𝐡𝐚𝐥𝐢 . 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+└───────────────⭓
+
+┌───────────────⭓
+│ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+├───────────────
+│ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
+│ https://fb./Murijhaw.con
+│ 💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽:
+│ https://wa.me/𝐬𝐢𝐦 𝐧𝐚𝐢 🙎‍♂️
+└───────────────⭓`;
 
     const cacheDir = path.join(__dirname, "cache");
     const imgPath = path.join(cacheDir, "owner.jpg");
 
     if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
 
-    const imgLink = "https://i.imgur.com/1G4ZhU7.jpeg";
+    const imgLink = "https://i.imgur.com/Va5BiK3.gif";
 
     const send = () => {
       api.sendMessage(
